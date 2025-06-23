2d8b7540d199da58c79b75cda7fef690775e3c89
Aman Gupta <amang4885@gmail.com>
2025-06-23 21:57:30 +0530

    Last changed by amann

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -32,6 +32,7 @@ class AdminController extends CI_Controller
   {
     header("Access-Control-Allow-Origin: *");       
     
+    
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, Authorization");
   }
